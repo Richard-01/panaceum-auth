2b9a0c48a23aabc41a2ef70049fab3e015c076a5
Richard <79211807+Richard-01@users.noreply.github.com>
2024-05-02 13:46:31 -0500

    Implemented full User module

--- src/modules/users/controllers/user.module.ts
+++ src/modules/users/controllers/user.module.ts
@@ -1,4 +1,45 @@
-import { Module } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Post,
+    Body,
+    Param,
+    Delete,
+    Put,
+} from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
+import { CreateUserDto, UpdateUserDto } from "../dto/user-index";
+import { UserService } from "../services/user.service";
 
-@Module({})
-export class ControllersModule {}
+
+@ApiBearerAuth()
+@ApiTags('users')
+@Controller('users')
+export class userConroller {
+    constructor(private readonly userService: UserService) { }
+
+    @Post()
+    create(@Body() CreateUserDto: CreateUserDto){
+        return this.userService.create(CreateUserDto);
+    }
+
+    @Get()
+    findAll(){
+        return this.userService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string){
+        return this.userService.findOne(id);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() UpdateUserDto: UpdateUserDto){
+        return this.userService.update(id, UpdateUserDto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string){
+        return this.userService.remove(id);
+    }
+}
--- src/modules/users/services/user.service.ts
+++ src/modules/users/services/user.service.ts
@@ -1,4 +1,65 @@
-import { Injectable } from '@nestjs/common';
+import {
+    HttpException,
+    HttpStatus,
+    Injectable,
+    NotFoundException,
+    ConflictException
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateUserDto, UpdateUserDto } from "../dto/user-index";
+import { User } from "../entities/user.entity";
 
 @Injectable()
-export class ServicesService {}
+export class UserService {
+    constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+    async create(createUserDto: CreateUserDto): Promise<User> {
+        const existingUser = await this.userModel.findOne({ email: createUserDto.email });
+        if (existingUser) {
+            throw new HttpException('User with that email already exists', HttpStatus.BAD_REQUEST);
+        }
+
+        const createdUser = new this.userModel(createUserDto);
+        return await createdUser.save();
+    }
+
+    async findAll(): Promise<User[]> {
+        return this.userModel.find();
+    }
+    
+    async findOne(id: string): Promise<User> {
+        const user = await this.userModel.findById(id);
+        if (!user) {
+          throw new NotFoundException(`User with id ${id} not found`);
+        }
+        return user;
+    }
+
+    async findUserByEmail(email: string, forRegistration: boolean = false): Promise<User> {
+        const user = await this.userModel.findOne({ email });
+        
+        if ((forRegistration && user) || (!forRegistration && !user)) {
+            throw forRegistration
+                ? new ConflictException(`User with email ${email} already exists`)
+                : new NotFoundException(`User with email ${email} not found`);
+        }
+        
+        return user;
+    }
+
+    async update(id: string, updateUserDto: UpdateUserDto): Promise<User> {
+        const updatedUser = await this.userModel.findByIdAndUpdate( id, updateUserDto, { new: true });
+        if (!updatedUser) {
+            throw new NotFoundException(`User with id ${id} not found`);
+        }
+        return updatedUser;
+    }
+
+    async remove(id: string): Promise<void> {
+        const deletedUser = await this.userModel.findByIdAndDelete(id);
+        if (!deletedUser) {
+            throw new NotFoundException(`User with id ${id} not found`);
+        }
+    }
+}
