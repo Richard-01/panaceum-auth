2da01a9d0cf30776ca2d89f1f43ac22ed0e4de86
MiguelTabares <68822677+MiguelTabares@users.noreply.github.com>
2024-05-14 06:59:05 -0500

    Update auth.module.ts

--- src/libs/auth/auth.module.ts
+++ src/libs/auth/auth.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
-import { ControllersModule } from './controllers/auth.controller.module';
-import { ServicesService } from './services/auth.service.service';
+import { AuthController } from './controllers/auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { UtilsModule } from '@libs/utils/utils.module';
+import { UsersModule } from '@modules/users/users.module';
+import { JwtStrategy } from './strategies/at.strategy';
+import { AuthService } from './services/auth.service';
 
 @Module({
-  imports: [ControllersModule],
-  providers: [ServicesService]
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.ACCESS_TOKEN_EXPIRY || '15m' },
+    }),
+    UtilsModule,
+    UsersModule,
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
