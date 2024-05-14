24c3ef11c11fb01571cdb2b2f3cd1fb2c71bcca8
MiguelTabares <68822677+MiguelTabares@users.noreply.github.com>
2024-05-14 07:13:56 -0500

    Update persistence.module.ts

--- src/libs/persistence/persistence.module.ts
+++ src/libs/persistence/persistence.module.ts
@@ -1,4 +1,24 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import dbConfig from './db-config';
 
-@Module({})
+@Global()
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigType<typeof dbConfig>) => {
+        const { db, env } = configService;
+        const uriDb =
+          env === "local"
+            ? `${db.connection}${db.host}/${db.name}`
+            : `${db.connection}${db.user}:${db.password}@${db.host}/${db.name}`;
+        return {
+          uri: uriDb,
+        };
+      },
+      inject: [dbConfig.KEY],
+    }),
+  ],
+})
 export class PersistenceModule {}
